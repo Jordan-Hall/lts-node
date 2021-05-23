11a6598d4ec84359b77b2c57e6f6e0cd065e0b73
Jordan Hall <Jordan@libertyware.co.uk>
2021-05-23 19:26:15 +0100

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projects: [
+    "<rootDir>/libs/common",
+    "<rootDir>/apps/tenant",
+    "<rootDir>/apps/accounts",
+    "<rootDir>/apps/billing",
+    "<rootDir>/apps/guardian",
+  ],
+};
