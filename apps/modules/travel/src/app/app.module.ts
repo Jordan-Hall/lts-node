e0e9aa86632efb51340c8e480132a2582f9af28a
Jordan Hall <Jordan@libertyware.co.uk>
2021-06-01 00:25:37 +0100

    feat: serverless nestjs

--- apps/modules/travel/src/app/app.module.ts
+++ apps/modules/travel/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { PermissionsModule } from '@ultimate-backend/permissions';
 import { CloudModule } from '@ultimate-backend/cloud';
 import { RedisModule } from '@ultimate-backend/redis';
 import { ConsulModule } from '@ultimate-backend/consul';
+import { ConsulModuleOptions } from '@ultimate-backend/consul/src/lib/consul-module.options';
+import { CONSUL_CONFIG_OPTIONS } from '@ultimate-backend/consul/src/lib/consul.constant';
 import { ClientModule } from '@ultimate-backend/client';
 import { LoadBalancerModule } from '@ultimate-backend/loadbalancer';
 import { BrakesModule } from '@ultimate-backend/brakes';
@@ -14,57 +16,58 @@ import { EtcdModule } from '@ultimate-backend/etcd';
 import * as path from 'path';
 @Module({
   imports: [
-    BootstrapModule.forRoot(),
-    GraphQLModule.forRoot({
-      autoSchemaFile: true,
+    BootstrapModule.forRoot({
+      enableEnv: true,
+      filePath: path.resolve(__dirname, './assets/bootstrap.yaml'),
     }),
     PermissionsModule.forRoot({
       polars: {
-        polar: path.resolve(__dirname, 'assets/example.polar'),
+        polar: path.resolve(__dirname, './assets/example.polar'),
         file: true,
       },
     }),
-    CloudModule.forRoot({
-      registry: {
-        discoverer: 'consul',
-        service: {
-          id: 'adv-travel',
-          port: 3332,
-          address: 'ec2-35-179-94-174.eu-west-2.compute.amazonaws.com',
-          name: 'adv-example',
-        },
-      },
-    }),
-    ConfigModule.forRoot({
-      load: [
-        {
-          source: ConfigSource.Consul,
-          key: 'ultimate-backend-config',
-        },
-      ],
-    }),
-    ConsulModule.forRoot({
-      host: 'ec2-35-179-94-174.eu-west-2.compute.amazonaws.com',
-      port: '8500',
-      debug: true,
-    } as any),
+    // CloudModule.forRoot({
+    //   registry: {
+    //     discoverer: 'consul',
+    //     service: {
+    //       id: 'adv-travel',
+    //       port: 3332,
+    //       address: 'https://consul-cluster.consul.6d7be6b7-7f0a-4d38-b0f9-2b545f2c4e05.aws.hashicorp.cloud',
+    //       name: 'adv-travel',
+    //     },
+    //   },
+    // }),
+    // ConfigModule.forRoot({
+    //   load: [
+    //     {
+    //       source: ConfigSource.Consul,
+    //       key: 'consul-cluster',
+    //     },
+    //   ],
+    // }),
+    // ConsulModule.forRoot({
+    //   host: 'https://consul-cluster.consul.6d7be6b7-7f0a-4d38-b0f9-2b545f2c4e05.aws.hashicorp.cloud',
+    //   port: '8500',
+    //   debug: true,
+    //   aclToken: 'b3cd3358-ee51-3b22-98e5-ea5537aa5f24'
+    // } as ConsulModuleOptions),
     RedisModule.forRoot({
       redisOptions: {
-        host: 'ec2-18-130-245-149.eu-west-2.compute.amazonaws.com',
+        host: '35.178.212.41',
         port: 6379,
         db: 0,
       },
     }),
-    ClientModule.forRoot(),
+    // ClientModule.forRoot(),
     EtcdModule.forRoot({
       etcdOptions: {
-        hosts: 'ec2-3-8-138-223.eu-west-2.compute.amazonaws.com:2379',
+        hosts: '18.134.241.48:2379',
       },
     }),
-    LoadBalancerModule.forRoot({
-      services: [{ strategy: 'RoundRobinStrategy', name: 'travel' }],
-    }),
+    // LoadBalancerModule.forRoot({
+    //   services: [{ strategy: 'RoundRobinStrategy', name: 'travel' }],
+    // }),
     BrakesModule.forRoot(),
-  ]
+  ],
 })
 export class AppModule {}
--- apps/modules/travel/src/main.ts
+++ apps/modules/travel/src/main.ts
@@ -1,15 +1,31 @@
 import { NestFactory } from "@nestjs/core";
 import { UBServiceFactory } from '@ultimate-backend/core';
 import { AppModule } from "./app/app.module";
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { INestApplication } from '@nestjs/common';
+import { Express } from 'express';
+import * as serverless from 'serverless-http';
+import { APIGatewayProxyEvent, APIGatewayProxyEventV2, Context } from "aws-lambda";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+let cachedServer: Express.Application;
+
+async function bootstrap(expressApp?: Express) {
+  let app: INestApplication
+
+  if (expressApp) {
+    app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  } else {
+    app = await NestFactory.create(AppModule);
+  }
+
   const whitelist = [
     'http://localhost:3000',
     'http://localhost:4000',
     'http://localhost:4200',
     'http://localhost:80',
     'http://localhost:8080',
+    'https://lts.sh',
+    'http://lts.sh',
   ]
   app.enableCors({
     origin: whitelist
@@ -27,6 +43,16 @@ async function bootstrap() {
     .withPrefix('api/v1')
     .withSwagger()
     .start();
+  return app;
 }
 
 (async () => await bootstrap())();
+
+
+
+export async function handler(event: APIGatewayProxyEvent | APIGatewayProxyEventV2, context: Context) {
+  if (!cachedServer) {
+    cachedServer = await bootstrap();
+  }
+  return serverless(cachedServer)(event, context)
+}
